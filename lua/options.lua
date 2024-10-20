7b38345783d6d55ef6483848ae00c9fe18103b7c
John Naeder <johnnaeder6537@gmail.com>
2024-10-20 13:54:04 +0700

    like above

--- lua/options.lua
+++ lua/options.lua
@@ -2,11 +2,20 @@ require("nvchad.options")
 
 local o = vim.o
 
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  o.shell = "powershell.exe"
+  o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  o.shellquote = ""
+  o.shellxquote = ""
+else
+  -- Set Fish shell for Linux or other systems
+  o.shell = "/usr/bin/fish"
+
 -- Indenting
 o.shiftwidth = 2
 o.tabstop = 2
 o.softtabstop = 2
 o.cursorlineopt = "both"
 o.relativenumber = true
-o.shell = "/usr/bin/fish"
+end
 o.wrap = false
